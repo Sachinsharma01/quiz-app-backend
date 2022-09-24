dd6a1f8a30ddd5da75ee6b7c0ad4b56fa9f37ade
Sachin Sharma <sachin2252sharma@gmail.com>
2022-09-25 02:31:34 +0530

    SS-02/error-classification-error-definationsons

--- src/config/APIResponses.ts
+++ src/config/APIResponses.ts
@@ -1,18 +1,28 @@
 import { Response } from "express";
+const setHeaders = (res: Response) => {
+  res.set({
+    "Content-Type": "application/json",
+  });
+};
 export default {
   success: (res: Response, message: string, data: object) => {
+    setHeaders(res);
     res.status(200).json({ error: false, message, data });
   },
   badRequest: (res: Response, message: string, data: object) => {
+    setHeaders(res);
     res.status(400).json({ error: true, message, data });
   },
   notFound: (res: Response, message: string, data: object) => {
+    setHeaders(res);
     res.status(404).json({ error: true, message, data });
   },
   internalServerError: (res: Response, message: string, data: object) => {
+    setHeaders(res);
     res.status(500).json({ error: true, message, data });
   },
   unAuthorized: (res: Response, message: string, data: object) => {
+    setHeaders(res);
     res.status(401).json({ error: true, message, data });
   },
 };
